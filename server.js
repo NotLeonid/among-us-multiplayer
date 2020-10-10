be52fa398ea55d902b22078b24cbb1e11000e319
KimPlayz4LK <leokim060109@gmail.com>
2020-10-10 18:18:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app=require('express')();
 var http=require('http').Server(app);
 var io=require('socket.io')(http);
-var port =3000;
+var port=3000;
 app.get('/',function(req,res){res.sendFile(__dirname + '/index.html');});
 app.get('*',function(req,res){res.sendFile(__dirname + req.originalUrl);});
 io.on('connection',socket =>{
@@ -10,4 +10,4 @@ socket.on('send-blue-position',data=>{socket.broadcast.emit('blue-position',data
 socket.on('send-green-position',data=>{socket.broadcast.emit('green-position',data);});
 socket.on('send-yellow-position',data=>{socket.broadcast.emit('yellow-position',data);});
 });
-http.listen(port,function(){console.log(`Among Us multiplayer server stared on port ${port}`);});
+http.listen(port,function(){console.log(`Among Us multiplayer server started on port ${port}`);});
