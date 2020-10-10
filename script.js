0122dd3972585a15ea13b557a775bcc3e6123a68
KimPlayz4LK <leokim060109@gmail.com>
2020-10-10 18:08:53 -0300

    Space save

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-const socket = io("http://10.0.0.47:3000");
-socket.on('red-position',pos=> {
-console.log(`%cRed ~ X: ${pos.x} Y: ${pos.y}`,"color:red;");
-red.x=pos.x;
-red.y=pos.y;
-});
-socket.on('blue-position',pos=> {
-console.log(`%cBlue ~ X: ${pos.x} Y: ${pos.y}`,"color:blue;");
-blue.x=pos.x;
-blue.y=pos.y;
-});
-socket.on('green-position',pos=> {
-console.log(`%cGreen ~ X: ${pos.x} Y: ${pos.y}`,"color:green;");
-green.x=pos.x;
-green.y=pos.y;
-});
-socket.on('yellow-position',pos=> {
-console.log(`%cYellow ~ X: ${pos.x} Y: ${pos.y}`,"color:yellow;");
-yellow.x=pos.x;
-yellow.y=pos.y;
-});
+const socket=io("https://among-us-multiplayer.herokuapp.com:3000");
+socket.on('red-position',pos=>{console.log(`%cRed ~ X: ${pos.x} Y: ${pos.y}`,"color:red;");red.x=pos.x;red.y=pos.y;});
+socket.on('blue-position',pos=>{console.log(`%cBlue ~ X: ${pos.x} Y: ${pos.y}`,"color:blue;");blue.x=pos.x;blue.y=pos.y;});
+socket.on('green-position',pos=>{console.log(`%cGreen ~ X: ${pos.x} Y: ${pos.y}`,"color:green;");green.x=pos.x;green.y=pos.y;});
+socket.on('yellow-position',pos=>{console.log(`%cYellow ~ X: ${pos.x} Y: ${pos.y}`,"color:yellow;");yellow.x=pos.x;yellow.y=pos.y;});
